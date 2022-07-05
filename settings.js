381d187bc69e122aba7f2456d727e9d4b0d32024
rusher12 <108649787+rusher12@users.noreply.github.com>
2022-07-05 11:27:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,32 +20,32 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is Dasun [TechGEEK]😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94703188412'] //Owner number in aive msg
+global.premium = ['+94703188412'] //Owner Number info
+global.ownernomer = '+94703188412' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '😎TECH GEEK[D]' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭Whatsapp📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'Whatsapp bot in all' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'Sri Lanka,Welimanda,Ambagasdowa' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = '😎Tech GEEK[D]❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Tech GEEK.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = ''Tech GEEK //Github Account!
 
-global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
+global.mess = Sinhala,English{ // COSTOMIZE WITH YOUR LANGUAGE
 	
     success: 'Done ✅',
     admin: 'Only Admins can use it!😉',
